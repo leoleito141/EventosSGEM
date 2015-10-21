00e016bc2b4d8a75a8dd472fe04031a79ea09e11
Leonardo <leonardo.cassanello@gmail.com>
2015-10-21 15:39:43 -0300

    Se emprolija registro y login de usuario.

--- EventosSGEM/WebContent/resources/defecto/js/controllers/usuarioCtrl.js
+++ EventosSGEM/WebContent/resources/defecto/js/controllers/usuarioCtrl.js
@@ -13,62 +13,90 @@ angular.module('eventosSGEM')
    
    $scope.comite={};
    
+   
+   $scope.mensajeValidacion = "";   
    $scope.altaUsuarioComun = function () {
-    
-      var usuarioComun = $scope.usuario;
+	  $scope.cargando = true;
+	  setTimeout( function(){	  
+			  
+			  
+			  var usuarioComun = $scope.usuario;
       usuarioComun.tenantId = dataTenant.tenantId;
       usuarioComun.tipoUsuario = "Comun";
       usuarioComun.password = btoa(usuarioComun.password);
       console.log("entre insertar" + usuarioComun);
-      
-   dataFactory.altaUsuarioComun(usuarioComun)
+       
+      dataFactory.altaUsuarioComun(usuarioComun)
       	.success(function (response, status, headers, config) {
               console.log(response);
               console.log(status);
               console.log(headers);
               console.log(config);
               if(response){
-	              event.preventDefault();
-	        	  $state.go('main', { tenant: $scope.nombreTenant});//le agrega el tenant a la url, para no tener que buscar devuelta el datatenant
+//            	  	event.preventDefault(); NO ANDA EN MOZILLA...
+					$state.go('login', { tenant: $scope.nombreTenant});//le agrega el tenant a la url, para no tener que buscar devuelta el datatenant
               }else{
-            	  alert("Error al dar de alta");
+            	  $scope.cargando = false;
+            	  $scope.mensajeValidacion = "Error al registrar usuario.";
               }
-      	}).error(function(error) {
+      	}).error(function(error,status) {
+      		
       		console.log(error);
-      		alert("Error al dar de alta");
+      		console.log(status);
+      		if(status == 302){
+      		  $scope.cargando = false;
+      		  $scope.mensajeValidacion = "El usuario con email: '"+usuarioComun.email + "' ya existe en el sistema.";
+      		}else{
+      		  $scope.cargando = false;
+      		  $scope.mensajeValidacion = "Error al registrar usuario.";
+      		}
       	});
+			  
+	  }, 1000) //espera 1 segundo
+     
       
    	};
    
    	
-   	$scope.mensajeValidacion = "Las credenciales no son correctas.";
-   	$scope.mostrarValidacion = false;
+//   	$scope.mensajeValidacion = "Las credenciales no son correctas.";
+//   	$scope.mostrarValidacion = false;
    	
    	$scope.loginUsuario = function () {
-   		
-   		var usuario = $scope.usuario;
-     
-        $auth.login({
-            email: usuario.email,
-            password: btoa(usuario.password) // base 64 
-        })
-        .then(function (data){           
-             $scope.usrLogin.email = $scope.usuario.email;
-             $scope.usuario.password="";
-             var payLoad = $auth.getPayload();   
-             console.log(payLoad);
-            
-             if(payLoad.rol == "Comun"){ 
-            	 // Ver bien si guardamos asi el rol en sesion, 
-            	 localStorage.setItem("rol", JSON.stringify(payLoad.rol));  // localStorage.getItem("rol") para obtenerlo
-            	 event.preventDefault();
-            	 $state.go('main', { tenant: $scope.nombreTenant});
-             }
-         })
-         .catch(function(response){
-         	console.log(response); 
-         	$scope.mostrarValidacion = true;
-         });
+   	     $scope.cargando = true;
+	   	 setTimeout( function(){	  
+	   		var usuario = $scope.usuario;
+	     
+	        $auth.login({
+	            email: usuario.email,
+	            password: btoa(usuario.password) // base 64 
+	        })
+	        .then(function (data){           
+	             $scope.usrLogin.email = $scope.usuario.email;
+	             $scope.usuario.password="";
+	             var payLoad = $auth.getPayload();   
+	             console.log(payLoad);
+	            
+	             if(payLoad.rol == "Comun"){ 
+	            	 // Ver bien si guardamos asi el rol en sesion, 
+	            	 localStorage.setItem("rol", JSON.stringify(payLoad.rol));  // localStorage.getItem("rol") para obtenerlo
+	            	 event.preventDefault();
+	            	 $state.go('main', { tenant: $scope.nombreTenant});
+	             }
+	         })
+	         .catch(function(error){
+	
+	       		console.log(error);
+	       		console.log(error.status);
+	       		if(error.status == 404){
+	       		  $scope.cargando = false;
+	       		  $scope.mensajeValidacion = "Email o contrase\u00F1a incorrecta.";
+	       		}else{
+	       		  $scope.cargando = false;
+	       		  $scope.mensajeValidacion = "Error al autenticar usuario.";
+	       		}
+	         });
+	        
+	    }, 1000) //espera 1 segundo
  	};
  	  
 	 $scope.altaComite = function(){
