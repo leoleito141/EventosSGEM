f4b1d3a7b34a5d0b976f78f3e0f97f77e0a2bcfe
Juanma <Juanma@sgem.com>
2015-11-20 20:39:13 -0300

    Duro el paypal !

--- EventosSGEM/WebContent/resources/defecto/js/controllers/perfilComiteCtrl.js
+++ EventosSGEM/WebContent/resources/defecto/js/controllers/perfilComiteCtrl.js
@@ -8,6 +8,25 @@ angular.module('eventosSGEM')
 //	  console.log(objetos.getObjeto());
 	  
 	  $scope.cargarDatos = function(){
+		  
+		  $.ajax({
+		        headers: {
+		             "Accept": "application/json",
+		             "Accept-Language": "en_US",
+		             "Authorization": "Basic "+btoa("AcD1onk8sS18COcbcOpl1gc9aUBOKMICfVfbYP9XFV7s1PlRox1gJ5SRpZN9EcPCDV8q_DryeS4MqLaU:EGLMTEBo-FyDb_bjsd7AhVEVXTeyfjNtxGgf0a_JF-soy3qE-9GCsTyKiqp8RhP4DiXB73Vgv-u-lqPL")
+		        },
+		        url: "https://api.sandbox.paypal.com/v1/oauth2/token",
+		        type: "POST",
+	            data: "grant_type=client_credentials",
+		        complete: function(result) {
+//		        	alert(JSON.stringify(result));
+		        	var token = result.responseJSON.access_token;
+		        	llamadaPaypal(token);
+		        },
+		});
+		  
+		  
+		  
 		  var existe = false;
 		  
 		  if((objetos.getObjeto() != null)&&(objetos.getObjeto().codigo !=null)){
@@ -48,8 +67,59 @@ angular.module('eventosSGEM')
 			  
 		}
 	
-	  
 
+		function llamadaPaypal(token){  
+			
+			var data = {};
+			data.intent = "sale";
+			
+			data.redirect_urls = {};
+			data.redirect_urls.return_url = "https://sgem-eventos.com:8443/#/Rio2016/";
+			data.redirect_urls.return_url = "https://google.com";
+			
+			data.payer = {};
+			data.payer.payment_method = "paypal";
+			data.transactions = [];
+			
+			var amount = {};
+			amount.total = 7.47;
+			amount.currency = "USD";
+			
+			data.transactions.push(amount);
+			
+			 $.ajax({
+			        headers: {
+			             "Content-Type": "application/json",
+			             "Authorization": "Bearer "+token
+			        },
+			        url: "https://api.sandbox.paypal.com/v1/payments/payment",
+			        type: "POST",
+		            data: 
+		            	{
+		            	  "intent":"sale",
+		            	  "redirect_urls":{
+		            	    "return_url":"http://<return URL here>",
+		            	    "cancel_url":"http://<cancel URL here>"
+		            	  },
+		            	  "payer":{
+		            	    "payment_method":"paypal"
+		            	  },
+		            	  "transactions":[
+		            	    {
+		            	      "amount":{
+		            	        "total":"7.47",
+		            	        "currency":"USD"
+		            	      },
+		            	      "description":"This is the payment transaction description."
+		            	    }
+		            	  ]
+		            	},
+			        complete: function(result) {
+			        	alert(JSON.stringify(result));
+			        	console.log(result);
+			        },
+			});
+		}
 	  
 
   }]);
